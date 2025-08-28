9a95af2c1e9a3b94de4cdae40964080ecc076ae4
NguyenHuuQuan-kamammite <quanhaha12@gmail.com>
2025-08-28 21:23:05 +0700

    shard explode

--- Baicuoiky/Assets/Scripts/Player/Player.cs
+++ Baicuoiky/Assets/Scripts/Player/Player.cs
@@ -137,6 +137,7 @@ public class Player : Entity
         input.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         input.Player.Movement.canceled += ctx => moveInput = Vector2.zero;
         input.Player.ToggleSkillTreeUI.performed += ctx => ui.ToggleSkillTree();
+        input.Player.Spell.performed += ctx => skillManager.shard.CreateShard();
     }
 
     private void OnDisable()
